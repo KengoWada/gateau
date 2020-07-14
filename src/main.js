d58643b9a7e9d25474dd41094572eaaae702859c
KengoWada <louiskengo3@gmail.com>
2020-07-14 21:43:29 +0300

    Added filter and vue-header component

--- src/main.js
+++ src/main.js
@@ -1,14 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify';
+import Vue from "vue";
+import Accounting from "accounting-js";
+import vueHeadful from "vue-headful";
 
-Vue.config.productionTip = false
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
+
+Vue.config.productionTip = false;
+
+Vue.component("vue-headful", vueHeadful);
+
+Vue.filter("currency", (val) => {
+  return Accounting.formatMoney(val, {
+    symbol: "UGX. ",
+    thousand: ",",
+  }).replace(".00", "");
+});
 
 new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
