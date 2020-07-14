e16847aa9e0c8c658ab2d0679729c97efb4654f4
KengoWada <louiskengo3@gmail.com>
2020-07-14 21:43:03 +0300

    Added snackbar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,33 @@
 <template>
   <v-app>
     <router-view></router-view>
+
+    <v-snackbar
+      v-model="snackbar.show"
+      :multi-line="true"
+      :right="true"
+      :top="true"
+      :timeout="5000"
+      :color="snackbar.variant"
+    >
+      {{ snackbar.message }}
+      <v-btn dark text @click="$store.commit('updateSnackbar', {show: false})">Close</v-btn>
+    </v-snackbar>
   </v-app>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+
+import { dbCakesRef } from "./firebase";
+
 export default {
-  name: "App"
+  name: "App",
+  computed: {
+    ...mapGetters(["snackbar"])
+  },
+  created() {
+    this.$store.dispatch("setCakesRef", dbCakesRef);
+  }
 };
 </script>
