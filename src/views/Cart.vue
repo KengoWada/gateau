33561b50daf463b4a19f13cc1e54b25c595a05ac
KengoWada <louiskengo3@gmail.com>
2020-07-14 21:45:55 +0300

    Added store and cart views

---
+++ src/views/Cart.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <vue-headful title="Cart | Gâteau" />
+
+    <TopNav />Cart
+    <homeFooter />
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+import Footer from "../components/Navs/Footer";
+import TopNav from "../components/Navs/TopNav";
+
+export default {
+  name: "Cart",
+  computed: {
+    ...mapGetters(["getCart"])
+  },
+  components: {
+    homeFooter: Footer,
+    TopNav
+  }
+};
+</script>
---
+++ src/views/Store.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <vue-headful title="Store | Gâteau" />
+
+    <TopNav />Store
+    <homeFooter />
+  </div>
+</template>
+
+<script>
+import Footer from "../components/Navs/Footer";
+import TopNav from "../components/Navs/TopNav";
+
+export default {
+  name: "Store",
+  components: {
+    homeFooter: Footer,
+    TopNav
+  }
+};
+</script>
