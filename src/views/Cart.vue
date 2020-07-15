26a75c2c90f9bdc18d99e93136305965dc694188
KengoWada <louiskengo3@gmail.com>
2020-07-15 09:58:08 +0300

    Updated cart view

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -2,7 +2,20 @@
   <div>
     <vue-headful title="Cart | Gâteau" />
 
-    <TopNav />Cart
+    <TopNav />
+    <v-container>
+      <v-row>
+        <v-col sm="8" md="5" offset-md="2" lg="4" offset-lg="3">
+          <ProductList />
+        </v-col>
+        <v-col sm="4" md="3" order="first" order-sm="last">
+          <CheckoutBox />
+        </v-col>
+      </v-row>
+    </v-container>
+    <br />
+    <br />
+    <br />
     <homeFooter />
   </div>
 </template>
@@ -12,6 +25,8 @@ import { mapGetters } from "vuex";
 
 import Footer from "../components/Navs/Footer";
 import TopNav from "../components/Navs/TopNav";
+import CheckoutBox from "../components/Cart/CheckoutBox";
+import ProductList from "../components/Cart/ProuctList";
 
 export default {
   name: "Cart",
@@ -20,7 +35,9 @@ export default {
   },
   components: {
     homeFooter: Footer,
-    TopNav
+    TopNav,
+    CheckoutBox,
+    ProductList
   }
 };
 </script>
