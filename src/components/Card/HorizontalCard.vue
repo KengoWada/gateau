a7d25582be6eca5be2e18a831114bd5239af5031
KengoWada <louiskengo3@gmail.com>
2020-07-15 09:57:31 +0300

    Added horizontal card view

--- src/components/Card/HorizontalCard.vue
+++ src/components/Card/HorizontalCard.vue
@@ -1,9 +1,43 @@
 <template>
-  <div>Horizontal Card</div>
+  <v-card outlined min-width="100%" class="mb-5 pa-4">
+    <div class="d-flex">
+      <div>
+        <v-img :src="product.image" height="130px" width="130px" />
+      </div>
+      <div class="d-flex flex-column justify-center">
+        <v-card-title class="pt-0">{{ product.name }}</v-card-title>
+        <v-card-subtitle>
+          <strong>
+            Quantity:
+            <v-btn color="success" small text @click="reduceQty(i)">
+              <v-icon small>fa-minus</v-icon>
+            </v-btn>
+            {{ product.quantity }}
+            <v-btn color="success" small text>
+              <v-icon small>fa-plus</v-icon>
+            </v-btn>
+          </strong>
+          <br />
+          {{ product.price * product.quantity | currency }}
+        </v-card-subtitle>
+        <v-card-actions>
+          <v-btn color="error" max-width="100%" class="ml-4" small outlined @click="updateCart(i)">
+            <v-icon left small>fa-minus</v-icon>Remove from cart
+          </v-btn>
+        </v-card-actions>
+      </div>
+    </div>
+  </v-card>
 </template>
 
 <script>
 export default {
-  name: "HorizontalCard"
+  name: "HorizontalCard",
+  props: {
+    product: Object,
+    updateCart: Function,
+    reduceQty: Function,
+    i: Number
+  }
 };
 </script>
