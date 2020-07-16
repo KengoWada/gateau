3cc9c53ef6a89225bab8b7c6babb0f1df0859572
KengoWada <louiskengo3@gmail.com>
2020-07-16 14:22:09 +0300

    Updated admin view

---
+++ src/views/Admin.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <vue-headful title="Admin | Gâteau" />
+    <TopNav />
+    <router-view></router-view>
+    <adminFooter />
+  </div>
+</template>
+
+<script>
+import TopNav from "../components/Navs/TopNav";
+import Footer from "../components/Navs/Footer";
+
+export default {
+  name: "Admin",
+  components: {
+    TopNav,
+    adminFooter: Footer
+  }
+};
+</script>
