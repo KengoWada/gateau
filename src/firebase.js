a0b7b3b28d3daf280b7ee4023630a9711d35b019
KengoWada <louiskengo3@gmail.com>
2020-07-16 14:20:47 +0300

    Added firebase auth

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
+import "firebase/auth";
 
 // Add Firebase credentials
 const config = {};
@@ -7,5 +8,6 @@ const config = {};
 firebase.initializeApp(config);
 const db = firebase.firestore();
 
+export const firebaseAuth = firebase.auth();
 export const dbCakesRef = db.collection("cakes");
 export const dbOrdersRef = db.collection("orders");
